7ba4fb9ee045f2fe6866dbe0132a206686b75f06
Sergio <zerjioang@users.noreply.github.com>
2017-05-08 19:00:44 +0200

    Update config.php

--- haproxy-example/config.php
+++ haproxy-example/config.php
@@ -3,6 +3,11 @@
 	$bd_usuario = "root";   
 	$bd_password = "d3m0";   
 	$bd_base = "example";   
-	$con = mysql_connect($bd_host, $bd_usuario, $bd_password);   
-	mysql_select_db($bd_base, $con);   
+	$con = @mysqli_connect($bd_host, $bd_usuario, $bd_password, $bd_base);
+
+	if (!$con) {
+		echo "Error: " . mysqli_connect_error();
+		exit();
+	}
+	echo 'Connected to MySQL';
 ?> 
