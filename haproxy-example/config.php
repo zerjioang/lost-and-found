75ed7be9284210526b465e341486d830e1aacbd6
Sergio <zerjioang@users.noreply.github.com>
2017-05-08 19:02:36 +0200

    Update config.php

--- haproxy-example/config.php
+++ haproxy-example/config.php
@@ -2,7 +2,7 @@
 	$bd_host = "localhost";   
 	$bd_usuario = "root";   
 	$bd_password = "d3m0";   
-	$bd_base = "example";   
+	$bd_base = "visitas";   
 	$con = @mysqli_connect($bd_host, $bd_usuario, $bd_password, $bd_base);
 
 	if (!$con) {
