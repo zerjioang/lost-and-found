7e39efd8bccbee70ebdef1f84544a9424e766d03
Sergio <zerjioang@users.noreply.github.com>
2017-05-10 15:28:20 +0200

    Update contador.sql

--- haproxy-example/contador.sql
+++ haproxy-example/contador.sql
@@ -1,10 +1,6 @@
-CREATE TABLE `contador` (   
-  `id` int(11) NOT NULL auto_increment,   
-  `ip` varchar(15) default NULL,   
-  `hora` varchar(8) default NULL,   
-  `fecha` varchar(20) default NULL,   
-  `horau` varchar(10) default NULL,   
-  `diau` char(3) default NULL,   
-  `aniou` varchar(4) default NULL,   
-  KEY `id` (`id`)   
-) TYPE=MyISAM AUTO_INCREMENT=1;
+CREATE TABLE IF NOT EXISTS visitas (
+    idvisitas int(7) not null auto_increment,
+    enlace varchar(300) not null,
+    visitas int(7) not null,
+    primary key (idvisitas)
+);
